69fa3ad3770b9cc0011b7f59752915ca4d20ca8f
Michael Fashola <michaelfashola@gmail.com>
2016-06-16 15:54:39 -0400

    Add a link to the Newest Album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -36,6 +36,11 @@ class AlbumsController < ApplicationController
     respond_with(@album)
   end
 
+  def newest_album
+    @album = Album(:created_at).last
+    respond_with(@album) #redirect_to :show
+  end
+
   private
     def set_album
       @album = Album.find(params[:id])
