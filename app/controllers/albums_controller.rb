7542a6a1da92b308b33f8e1b0c00b88b93aa2dcf
Michael Fashola <michaelfashola@gmail.com>
2016-06-29 14:51:13 -0400

    Refactor cache feature

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -25,48 +25,61 @@ class AlbumsController < ApplicationController
   def create
     @album = Album.new(album_params)
     if @album.save
-      if Album.get(:most_recent).size < 4
-        Album.set(:most_recent, Album.get(:most_recent).to_a.push(@album) )
-      else
-        new_store = []
-        Album.get(:most_recent).to_a.each do |album|
-          new_store << album unless Date.strptime(album.released_on, '%Y-%m-%d') > Date.strptime(@album.released_on, '%Y-%m-%d')
-        end
-
-        if new_store.size < 4
-          new_store.push(obj)
-        end
-        Album.set(:most_recent, new_store)
-      end
+      Album.consider_add('most_recent',@album)
     end
     respond_with(@album)
   end
 
+  # def create
+  #   @album = Album.new(album_params)
+  #   if @album.save
+  #     if Album.get(:most_recent).size < 4
+  #       Album.set(:most_recent, Album.get(:most_recent).to_a.push(@album) )
+  #     else
+  #       new_store = []
+  #       Album.get(:most_recent).to_a.each do |album|
+  #         new_store << album unless Date.strptime(album.released_on, '%Y-%m-%d') > Date.strptime(@album.released_on, '%Y-%m-%d')
+  #       end
+  #
+  #       if new_store.size < 4
+  #         new_store.push(obj)
+  #       end
+  #       Album.set(:most_recent, new_store)
+  #     end
+  #   end
+  #   respond_with(@album)
+  # end
   def update
     if @album.update(album_params)
-      old_store = Album.get(:most_recent)
-      if old_store.size < 4
-        old_store.to_a.each_with_index do |album, ind|
-          if @album.id == album.id
-            old_store.delete_at(ind)
-            old_store.push(@album).sort_by &:released_on
-          end
-        end
-        Album.set(:most_recent, old_store )
-      else
-        new_store = []
-        Album.get(:most_recent).to_a.each do |album|
-          new_store << album unless Date.strptime(album.released_on, '%Y-%m-%d') > Date.strptime(@album.released_on, '%Y-%m-%d')
-        end
-
-        if new_store.size < 4
-          new_store.push(@album)
-        end
-        Album.set(:most_recent, new_store)
-      end
+      Album.consider_add('most_recent',@album)
     end
     respond_with(@album)
   end
+  # def update
+  #   if @album.update(album_params)
+  #     old_store = Album.get(:most_recent)
+  #     if old_store.size < 4
+  #       old_store.to_a.each_with_index do |album, ind|
+  #         if @album.id == album.id
+  #           old_store.delete_at(ind)
+  #           old_store.push(@album).sort_by &:released_on
+  #         end
+  #       end
+  #       Album.set(:most_recent, old_store )
+  #     else
+  #       new_store = []
+  #       Album.get(:most_recent).to_a.each do |album|
+  #         new_store << album unless Date.strptime(album.released_on, '%Y-%m-%d') > Date.strptime(@album.released_on, '%Y-%m-%d')
+  #       end
+  #
+  #       if new_store.size < 4
+  #         new_store.push(@album)
+  #       end
+  #       Album.set(:most_recent, new_store)
+  #     end
+  #   end
+  #   respond_with(@album)
+  # end
 
   def destroy
     @album.destroy
--- app/models/album.rb
+++ app/models/album.rb
@@ -18,13 +18,43 @@ class Album < ActiveRecord::Base
     order(released_on: :asc).reverse_order.limit(num_albums).reverse
   end
 
+  def self.consider_add(key, value)
+    old_store = Album.get(key)
+    #raise old_store.inspect
+    # determine if album is already in collection. If it is, swap it with new, else add it
+    old_store.to_a.each_with_index do |album, ind|
+      if value.id == album.id
+        old_store.delete_at(ind)
+        old_store.push(value)
+        break
+      else
 
-  def self.set(key, value)
-    @@datastore[key] = value
+      end
+    end
+
+    old_store.to_a.push(value) unless old_store.to_a.include?(value)
+
+    # raise old_store.size.inspect
+    # remove old record that no longer belongs in collection
+    if old_store.to_a.size > 4
+      old_store = old_store.to_a.sort_by { |k| k[:released_on] } #&:released_on
+      old_store.to_a.pop
+    else
+    end
+
+    # save the new collection
+    Album.set(:most_recent, old_store )
+
   end
 
   def self.get(key)
     @@datastore[key] ||= Album.most_recent
   end
+
+  private
+
+    def self.set(key, value)
+      @@datastore[key] = value
+    end
 
 end
